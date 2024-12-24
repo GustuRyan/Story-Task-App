f37716b0479b3f3ae2dd40592f41b740f3340f55
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2024-12-24 13:47:52 +0800

    update MainActivity

--- app/src/main/java/com/example/storyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/storyapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.storyapp.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.example.storyapp.R
@@ -20,7 +21,10 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         val sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
-        val isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false)  // Cek status login
+        val isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = android.graphics.Color.TRANSPARENT
 
         if (isLoggedIn) {
             navController.navigate(R.id.homeFragment)
